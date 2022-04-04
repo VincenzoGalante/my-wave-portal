cff1944a2fdd4f5b384e1a32d895c6a9ba47c9d0
Vincenzo Galante <mr.vincenzo.galante@gmail.com>
2022-04-04 23:36:24 +0200

    push 01

---
+++ scripts/run.js
@@ -0,0 +1,8 @@
+const { hexStripZeros } = require("ethers/lib/utils");
+
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log("Contract deployed to:", waveContract.address);
+};
