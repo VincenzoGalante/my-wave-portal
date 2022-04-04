f94013cc4f44a76880b184c7451e1d1724dda3f0
Vincenzo Galante <mr.vincenzo.galante@gmail.com>
2022-04-04 23:39:51 +0200

    Push 02

--- scripts/run.js
+++ scripts/run.js
@@ -6,3 +6,13 @@ const main = async () => {
   await waveContract.deployed();
   console.log("Contract deployed to:", waveContract.address);
 };
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
